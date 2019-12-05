2fba38cc8661a44b5c3211076ac5b7ddb8e78526
Magesh Kuppan <tkmagesh77@gmail.com>
2019-12-05 15:22:23 +0530

    calculator solution

--- first-app/src/app/calculator/calculator.component.ts
+++ first-app/src/app/calculator/calculator.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
     styleUrls : ['calculator.component.css']
 })
 export class CalculatorComponent{
+    n1 : number = 0;
+    n2 : number = 0;
+    result : number = 0;
 
+    setN1(value){
+        this.n1 = value;
+    }
+
+    setN2(value){
+        this.n2 = value;
+    }
+    onAddClick(){
+        this.result = this.n1 + this.n2;
+    }
+    onSubtractClick(){
+        this.result = this.n1 - this.n2;
+    }
+    onMultiplyClick(){
+        this.result = this.n1 * this.n2;
+    }
+    onDivideClick(){
+        this.result = this.n1 / this.n2;
+    }
 }
